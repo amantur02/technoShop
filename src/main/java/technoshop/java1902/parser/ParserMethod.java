8e76cb97b3d85319b4d9624cded9520e952eba7c
Amantur-344 <amanamanovica@gmail.com>
2020-06-07 19:20:59 +0300

    add new class

---
+++ src/main/java/technoshop/java1902/parser/ParserMethod.java
@@ -0,0 +1,8 @@
+package technoshop.java1902.parser;
+
+public class ParserMethod {
+    public static String addString(String add, String str){
+        String example = add + str;
+        return example;
+    }
+}
