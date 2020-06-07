64b68b77792a794413652e856f2ea0fb7441e309
Amantur-344 <amanamanovica@gmail.com>
2020-06-07 13:21:09 +0300

    test commit

--- src/main/java/technoshop/java1902/Java1902Application.java
+++ src/main/java/technoshop/java1902/Java1902Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Java1902Application {
+public class Java1902Application { 
 
     public static void main(String[] args) {
         SpringApplication.run(Java1902Application.class, args);
